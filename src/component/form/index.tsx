6c1c72378c1ab17334c45bb95fe4fd2661392408
KingFish <891248330@qq.com>
2020-06-30 18:26:55 +0800

    更新

--- src/component/form/index.tsx
+++ src/component/form/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { observer, inject } from 'mobx-react'
+import { toJS } from 'mobx'
 import './index.less'
 @inject('UI')
 @observer
@@ -8,9 +9,111 @@ class Form extends React.Component<any, any> {
   constructor(props) {
     super(props)
   }
-  render() {
+  render() { 
+    const { form, setFormByKey } = this.props.UI
     return <div className='app-form'>
-      app-form
+      <div><input placeholder='请输入姓名' value={form.name}
+        onChange={
+          (e) => {
+            setFormByKey('name', e.target.value)
+
+          }
+        }
+      />
+      </div>
+      <div>
+        <input placeholder='请输入年龄' value={form.age}
+          onChange={
+            (e) => {
+              setFormByKey('age', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入爱好' value={form.like}
+          onChange={
+            (e) => {
+              setFormByKey('like', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入班级' value={form.Class}
+          onChange={
+            (e) => {
+              setFormByKey('Class', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入Type' value={form.Type}
+          onChange={
+            (e) => {
+              setFormByKey('Type', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入主题' value={form.topic}
+          onChange={
+            (e) => {
+              setFormByKey('topic', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入strert' value={form.strert}
+          onChange={
+            (e) => {
+              setFormByKey('strert', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入城市' value={form.city}
+          onChange={
+            (e) => {
+              setFormByKey('city', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <input placeholder='请输入地区' value={form.area}
+          onChange={
+            (e) => {
+              setFormByKey('area', e.target.value)
+
+            }
+          }
+        />
+      </div>
+      <div>
+        <button onClick={
+          () => {
+            console.log(toJS(form))
+          }
+        }>提交</button>
+        <button onClick={
+          () => {
+            // 便利属性 ->复制=3
+            
+          }
+        }>查询</button>
+      </div>
     </div>
   }
 }
--- src/store/ui/index.ts
+++ src/store/ui/index.ts
@@ -8,6 +8,23 @@ class UI {
   @action setVisable = (visable) => {
     this.visable = visable
   }
+  /**
+   * 定义表单
+   */
+  @observable form = {
+    name: '',
+    age: '',
+    like: '',
+    Class: '',
+    Type: '',
+    topic: '',
+    strert: '',
+    city: '',
+    area: ''
+  }
+  @action setFormByKey = (key: string, value: any) => {
+    this.form[key] = value
+  }
 }
 const ui = new UI()
 export {
